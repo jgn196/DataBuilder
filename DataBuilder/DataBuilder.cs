56495b8a4961452cc9de03e88267759ec5d1726e
John Nash <john.nash@capgemini.com>
2014-12-29 11:19:31 +0000

    Copies and concatenates data.
    The DataBuilder can copy and concatenate byte arrays.

--- DataBuilder/DataBuilder.cs
+++ DataBuilder/DataBuilder.cs
@@ -1,26 +1,52 @@
-﻿using System;
+﻿using CuttingEdge.Conditions;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Capgemini.DataBuilder
 {
     /// <summary>
     /// A utility class for making writing small binary data samples easy.
     /// 
-    /// Clients instantiate an instance of the builder, call various methods in the Fluent style to configure how data will be generated
-    /// and then call the Build method to build the data.
+    /// Clients instantiate an instance of the builder, call various methods in the Fluent style to configure 
+    /// how data will be generated and then call the Build method to build the data.
+    /// The order in which methods are called is the order in which their arguments will be added to the 
+    /// finished built data.
     /// </summary>
     public class DataBuilder
     {
+        private IList<byte[]> recipeElements = new List<byte[]>();
+
+        /// <summary>
+        /// Adds an arbitary chunk of binary data to the build "recipe".
+        /// </summary>
+        /// <param name="value">The data that should be copied into the recipe.</param>
+        /// <returns>This DataBuilder for chaining calls.</returns>
+        public DataBuilder Append(byte[] value)
+        {
+            Condition.Ensures(value).IsNotNull();
+
+            recipeElements.Add((byte[]) value.Clone());
+
+            return this;
+        }
+
         /// <summary>
         /// Builds the data according to the "recipe" that has been built up by calling methods on this object.
         /// </summary>
         /// <returns>The new data.</returns>
         public byte[] Build()
         {
-            return new byte[0];
+            IEnumerable<byte> result = new byte[0];
+
+            foreach (byte[] element in recipeElements)
+            {
+                result = result.Concat(element);
+            }
+
+            return result.ToArray();
         }
     }
 }
--- DataBuilderTests/DataBuilderTests.cs
+++ DataBuilderTests/DataBuilderTests.cs
@@ -22,5 +22,34 @@ namespace DataBuilderTests
         {
             Assert.AreEqual(0, new DataBuilder().Build().Length);
         }
+
+        /// <summary>
+        /// Tests that you can use the DataBuilder to copy a byte array.
+        /// </summary>
+        [TestMethod]
+        public void CopyData()
+        {
+            byte[] source = new byte[] { 1, 2, 3 };
+
+            byte[] result = new DataBuilder().Append(source).Build();
+
+            Assert.AreNotSame(source, result);
+            Assert.IsTrue(Enumerable.SequenceEqual(source, result));
+        }
+
+        /// <summary>
+        /// Tests that you can use the DataBuilder to concatenate byte arrays together.
+        /// </summary>
+        [TestMethod]
+        public void ConcatenateData()
+        {
+            byte[] source1 = new byte[] { 1, 2, 3 };
+            byte[] source2 = new byte[] { 4, 5, 6 };
+
+            byte[] result = new DataBuilder().Append(source1).Append(source2).Build();
+
+            Assert.AreEqual(6, result.Length);
+            Assert.IsTrue(Enumerable.Range(1, 6).SequenceEqual(result.Select(b => (int)b)));
+        }
     }
 }
