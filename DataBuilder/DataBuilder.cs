f9dd83a30c6988be7259dfb8a39c187f4b42608c
John Nash <john.nash@capgemini.com>
2014-12-29 10:41:49 +0000

    Builds empty data sets.
    Added the Build method and a unit test for build empty data sets.

---
+++ DataBuilder/DataBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capgemini.DataBuilder
+{
+    /// <summary>
+    /// A utility class for making writing small binary data samples easy.
+    /// 
+    /// Clients instantiate an instance of the builder, call various methods in the Fluent style to configure how data will be generated
+    /// and then call the Build method to build the data.
+    /// </summary>
+    public class DataBuilder
+    {
+        /// <summary>
+        /// Builds the data according to the "recipe" that has been built up by calling methods on this object.
+        /// </summary>
+        /// <returns>The new data.</returns>
+        public byte[] Build()
+        {
+            return new byte[0];
+        }
+    }
+}
---
+++ DataBuilderTests/DataBuilderTests.cs
@@ -0,0 +1,26 @@
+﻿using Capgemini.DataBuilder;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBuilderTests
+{
+    /// <summary>
+    /// Tests the DataBuilder class.
+    /// </summary>
+    [TestClass]
+    public class DataBuilderTests
+    {
+        /// <summary>
+        /// Tests building an empty (zero length) data set.
+        /// </summary>
+        [TestMethod]
+        public void BuildEmptyData()
+        {
+            Assert.AreEqual(0, new DataBuilder().Build().Length);
+        }
+    }
+}
